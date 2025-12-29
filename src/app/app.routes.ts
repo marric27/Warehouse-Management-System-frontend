da03b199a1343f38e1085e565c339cd74a8376c3
marric27 <marco.ricci796@gmail.com>
2025-12-29 17:46:13 +0100

    create first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Products } from './components/products/products';
+import { Customers } from './components/customers/customers';
+import { Warehouse } from './components/warehouse/warehouse';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'products', component: Products },
+    { path: 'customers', component: Customers },
+    { path: 'warehouse', component: Warehouse }
+];
