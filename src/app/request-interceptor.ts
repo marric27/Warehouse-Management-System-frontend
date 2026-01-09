bd239b7ebb0c4d62ae0d67ced8e114eb4a3bc633
marric27 <marco.ricci796@gmail.com>
2026-01-09 13:00:41 +0100

    if not logged redirect to login

--- src/app/request-interceptor.ts
+++ src/app/request-interceptor.ts
@@ -1,14 +1,25 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const requestInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   const token = localStorage.getItem('authToken');
 
   if (token && !req.url.includes('/login')) {
     req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
+      setHeaders: { Authorization: `Bearer ${token}` }
     });
   }
-  return next(req);
+
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem('authToken'); // pulisci token
+        router.navigate(['/login']);          // rimanda al login
+      }
+      return throwError(() => error);
+    })
+  );
 };
